e44d7eb10a15665c9ee91f2f5e02426b81c5c373
Sergey Yaremych <sergeyfitis@gmail.com>
2023-07-06 00:01:08 +0300

    reformat shader code

--- app/src/main/assets/shader/Renderer2D_Circle.glsl
+++ app/src/main/assets/shader/Renderer2D_Circle.glsl
@@ -1,35 +1,35 @@
 #type vertex
 #version 310 es
- precision mediump float;
+precision mediump float;
 
-layout (std140, binding = 0) uniform Camera
+layout(std140,binding=0)uniform Camera
 {
-mat4 u_ViewProjection;
+    mat4 u_ViewProjection;
 };
 
-layout (location = 0) in vec3 a_WorldPosition;
-layout (location = 1) in vec2 a_LocalPosition;
-layout (location = 2) in vec4 a_FillColor;
-layout (location = 3) in vec4 a_StrokeColor;
-layout (location = 4) in float a_StrokeWidth;
+layout(location=0)in vec3 a_WorldPosition;
+layout(location=1)in vec2 a_LocalPosition;
+layout(location=2)in vec4 a_FillColor;
+layout(location=3)in vec4 a_StrokeColor;
+layout(location=4)in float a_StrokeWidth;
 //layout (location = 5) in float a_FlipTexture;
 
 struct VertexOutput
 {
-vec2 LocalPosition;
-vec4 FillColor;
-vec4 StrokeColor;
-float StrokeWidth;
+    vec2 LocalPosition;
+    vec4 FillColor;
+    vec4 StrokeColor;
+    float StrokeWidth;
 };
 
-layout (location = 0) out VertexOutput Output;
+layout(location=0)out VertexOutput Output;
 
-void main() {
-Output.LocalPosition = a_LocalPosition;
-Output.FillColor = a_FillColor;
-Output.StrokeColor = a_StrokeColor;
-Output.StrokeWidth = a_StrokeWidth;
-gl_Position = u_ViewProjection * vec4(a_WorldPosition, 1.0);
+void main(){
+    Output.LocalPosition=a_LocalPosition;
+    Output.FillColor=a_FillColor;
+    Output.StrokeColor=a_StrokeColor;
+    Output.StrokeWidth=a_StrokeWidth;
+    gl_Position=u_ViewProjection*vec4(a_WorldPosition,1.);
 }
 
 #type fragment
@@ -39,52 +39,54 @@ precision mediump float;
 
 struct VertexOutput
 {
-vec2 LocalPosition;
-vec4 FillColor;
-vec4 StrokeColor;
-float StrokeWidth;
+    vec2 LocalPosition;
+    vec4 FillColor;
+    vec4 StrokeColor;
+    float StrokeWidth;
 };
 
-layout (location = 0) in VertexOutput Input;
-layout (location = 0) out vec4 color;
+layout(location=0)in VertexOutput Input;
+layout(location=0)out vec4 color;
 //layout (binding = 0) uniform sampler2D u_Textures[8];
 
-float sdfCircle(vec2 localPosition, float thickness, float fade) {
-float distance = 1.0 - length(localPosition);
-float circle = smoothstep(0.0, fade, distance);
-circle *= smoothstep(thickness + fade, thickness, distance);
-return circle;
+float sdfCircle(vec2 localPosition,float thickness,float fade){
+    float distance=1.-length(localPosition);
+    float circle=smoothstep(0.,fade,distance);
+    circle*=smoothstep(thickness+fade,thickness,distance);
+    return circle;
 }
 
-vec4 sdfCircle2(vec2 localPosition, vec4 fillColor, vec4 strokeColor, float strokeWidth) {
-float radius = 1.0;
-
-float dist = length(localPosition);
-float aa = fwidth(dist);
-
-// Create the SDF
-float circleSDF = radius - dist;
-
-// Calculate the smooth edges of the stroke
-float strokeEdge = smoothstep(aa, - aa, circleSDF - strokeWidth);
-
-// Calculate the smooth edges of the circle
-float circleEdge = smoothstep(aa, - aa, circleSDF);
-
-// Calculate the color of the pixel
-vec4 c = mix(fillColor, strokeColor, strokeEdge);
-
-// Apply the circle edge to the alpha
-c.a *= 1.0 - circleEdge;
-return c;
+vec4 sdfCircle2(vec2 localPosition,vec4 fillColor,vec4 strokeColor,float strokeWidth){
+    float radius=1.;
+    
+    float dist=length(localPosition);
+    float aa=fwidth(dist);
+    
+    // Create the SDF
+    float circleSDF=radius-dist;
+    
+    // Calculate the smooth edges of the stroke
+    float strokeEdge=smoothstep(aa,-aa,circleSDF-strokeWidth);
+    
+    // Calculate the smooth edges of the circle
+    float circleEdge=smoothstep(aa,-aa,circleSDF);
+    
+    // Calculate the color of the pixel
+    vec4 c=mix(fillColor,strokeColor,strokeEdge);
+    
+    // Apply the circle edge to the alpha
+    c.a*=1.-circleEdge;
+    return c;
 }
 
-void main() {
-vec4 circle = sdfCircle2(Input.LocalPosition, Input.FillColor, Input.StrokeColor, Input.StrokeWidth);
-if (circle.a == 0.0)
-discard;
-
-// color = Input.Color;
-// color.a *= circle;
-color = circle;
+void main(){
+    vec4 circle=sdfCircle2(Input.LocalPosition,Input.FillColor,Input.StrokeColor,Input.StrokeWidth);
+    if(circle.a==0.){
+        // color=vec4(1.0, 0.0, 1.0, 1.0);
+        discard;
+    }else{
+        // color = Input.Color;
+        // color.a *= circle;
+        color=circle;
+    }
 }
--- app/src/main/assets/shader/Renderer2D_Quad.glsl
+++ app/src/main/assets/shader/Renderer2D_Quad.glsl
@@ -1,37 +1,37 @@
 #type vertex
 #version 310 es
- precision mediump float;
+precision mediump float;
 
-layout (std140, binding = 0) uniform Camera
+layout(std140,binding=0)uniform Camera
 {
-mat4 u_ViewProjection;
+    mat4 u_ViewProjection;
 };
 
-layout (location = 0) in vec3 a_Position;
-layout (location = 1) in vec2 a_TexCoord;
-layout (location = 2) in vec4 a_Color;
-layout (location = 3) in float a_TexIndex;
-layout (location = 4) in float a_TilingFactor;
-layout (location = 5) in float a_FlipTexture;
+layout(location=0)in vec3 a_Position;
+layout(location=1)in vec2 a_TexCoord;
+layout(location=2)in vec4 a_Color;
+layout(location=3)in float a_TexIndex;
+layout(location=4)in float a_TilingFactor;
+layout(location=5)in float a_FlipTexture;
 
 struct VertexOutput
 {
-vec4 Color;
-vec2 TexCoord;
-float TexIndex;
-float TilingFactor;
-float FlipTexture;
+    vec4 Color;
+    vec2 TexCoord;
+    float TexIndex;
+    float TilingFactor;
+    float FlipTexture;
 };
 
-layout (location = 0) out VertexOutput Output;
+layout(location=0)out VertexOutput Output;
 
-void main() {
-Output.Color = a_Color;
-Output.TexCoord = a_TexCoord;
-Output.TexIndex = a_TexIndex;
-Output.TilingFactor = a_TilingFactor;
-Output.FlipTexture = a_FlipTexture;
-gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+void main(){
+    Output.Color=a_Color;
+    Output.TexCoord=a_TexCoord;
+    Output.TexIndex=a_TexIndex;
+    Output.TilingFactor=a_TilingFactor;
+    Output.FlipTexture=a_FlipTexture;
+    gl_Position=u_ViewProjection*vec4(a_Position,1.);
 }
 
 #type fragment
@@ -41,42 +41,42 @@ precision mediump float;
 
 struct VertexOutput
 {
-vec4 Color;
-vec2 TexCoord;
-float TexIndex;
-float TilingFactor;
-float FlipTexture;
+    vec4 Color;
+    vec2 TexCoord;
+    float TexIndex;
+    float TilingFactor;
+    float FlipTexture;
 };
 
-layout (location = 0) in VertexOutput Input;
-layout (location = 0) out vec4 color;
-layout (binding = 0) uniform sampler2D u_Textures[8];
+layout(location=0)in VertexOutput Input;
+layout(location=0)out vec4 color;
+layout(binding=0)uniform sampler2D u_Textures[8];
 
-void main() {
-vec4 sum = vec4(1.0);
-bool flipTexture = int(Input.FlipTexture) > 0;
-vec2 texCoord = flipTexture ? vec2(Input.TexCoord.x, 1.0 - Input.TexCoord.y): Input.TexCoord;
-// pre OpenGL ES 3.1 arrays of samplers may only be indexed by a constant integral expression
-switch (int(Input.TexIndex)) {
-case 0:
-sum = texture(u_Textures[0], texCoord * Input.TilingFactor); break;
-case 1:
-sum = texture(u_Textures[1], texCoord * Input.TilingFactor); break;
-case 2:
-sum = texture(u_Textures[2], texCoord * Input.TilingFactor); break;
-case 3:
-sum = texture(u_Textures[3], texCoord * Input.TilingFactor); break;
-case 4:
-sum = texture(u_Textures[4], texCoord * Input.TilingFactor); break;
-case 5:
-sum = texture(u_Textures[5], texCoord * Input.TilingFactor); break;
-case 6:
-sum = texture(u_Textures[6], texCoord * Input.TilingFactor); break;
-case 7:
-sum = texture(u_Textures[7], texCoord * Input.TilingFactor); break;
-}
-// OpenGL 3.1+
-//    color = texture(u_Textures[int(Input.TexIndex)], vec2(Input.TexCoord.x, 1.0 - Input.TexCoord.y)) * Input.Color;
-color = sum * Input.Color;
-//    color = vec4(Input.TilingFactor, Input.TilingFactor, Input.TilingFactor, 1.0);
+void main(){
+    vec4 sum=vec4(1.);
+    bool flipTexture=int(Input.FlipTexture)>0;
+    vec2 texCoord=flipTexture?vec2(Input.TexCoord.x,1.-Input.TexCoord.y):Input.TexCoord;
+    // pre OpenGL ES 3.1 arrays of samplers may only be indexed by a constant integral expression
+    switch(int(Input.TexIndex)){
+        case 0:
+        sum=texture(u_Textures[0],texCoord*Input.TilingFactor);break;
+        case 1:
+        sum=texture(u_Textures[1],texCoord*Input.TilingFactor);break;
+        case 2:
+        sum=texture(u_Textures[2],texCoord*Input.TilingFactor);break;
+        case 3:
+        sum=texture(u_Textures[3],texCoord*Input.TilingFactor);break;
+        case 4:
+        sum=texture(u_Textures[4],texCoord*Input.TilingFactor);break;
+        case 5:
+        sum=texture(u_Textures[5],texCoord*Input.TilingFactor);break;
+        case 6:
+        sum=texture(u_Textures[6],texCoord*Input.TilingFactor);break;
+        case 7:
+        sum=texture(u_Textures[7],texCoord*Input.TilingFactor);break;
+    }
+    // OpenGL 3.1+
+    //    color = texture(u_Textures[int(Input.TexIndex)], vec2(Input.TexCoord.x, 1.0 - Input.TexCoord.y)) * Input.Color;
+    color=sum*Input.Color;
+    //    color = vec4(Input.TilingFactor, Input.TilingFactor, Input.TilingFactor, 1.0);
 }
